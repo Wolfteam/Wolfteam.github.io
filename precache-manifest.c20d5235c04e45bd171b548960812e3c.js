30597d11ccdbc3285945bad2d6f77b25d2627b28
Efrain Bastidas <mimo4325@gmail.com>
2020-09-27 22:12:33 -0500

    Updates

---
+++ precache-manifest.c20d5235c04e45bd171b548960812e3c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86fad0b5f1d630e60c19c081073d7e6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9212f0be18a61dff32f7",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "a178ee957bdcfded8988",
+    "url": "/static/css/main.b40de77b.chunk.css"
+  },
+  {
+    "revision": "9212f0be18a61dff32f7",
+    "url": "/static/js/2.96934997.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.96934997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a178ee957bdcfded8988",
+    "url": "/static/js/main.398d5231.chunk.js"
+  },
+  {
+    "revision": "35f710bd27dbd3ad6075",
+    "url": "/static/js/runtime-main.fc828d1e.js"
+  },
+  {
+    "revision": "f24b778f3d52714cbc7db180d8c81888",
+    "url": "/static/media/casit.f24b778f.png"
+  },
+  {
+    "revision": "cc21d65a246542d885a604119623d9b6",
+    "url": "/static/media/downloads-bg.cc21d65a.jpg"
+  },
+  {
+    "revision": "23c893eaa8caee50a6d736679fdc4374",
+    "url": "/static/media/intro-bg.23c893ea.jpg"
+  },
+  {
+    "revision": "dffd97a8c9ec935b74491d7780b6ee68",
+    "url": "/static/media/mirainotes.dffd97a8.png"
+  },
+  {
+    "revision": "f5897f654a58665678de977699332852",
+    "url": "/static/media/myexpenses.f5897f65.png"
+  },
+  {
+    "revision": "338b4708168d11d84d48734d9626de49",
+    "url": "/static/media/tibia_info.338b4708.png"
+  }
+]);
